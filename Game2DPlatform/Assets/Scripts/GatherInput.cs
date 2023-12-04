1ecb30b76cf1080844f1d102a0213e5ef46f1769
fors3tti <152114883+Fors3tti@users.noreply.github.com>
2023-12-03 22:39:13 -0400

    Created Gather Input Config Script

---
+++ Game2DPlatform/Assets/Scripts/GatherInput.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class GatherInput : MonoBehaviour
+{
+    private Controls myControls;
+
+    public float valueX;
+
+    private void Awake()
+    {
+        myControls = new Controls();
+    }
+
+    private void OnEnable()
+    {
+        myControls.Player.Move.performed += StartMove;
+
+        myControls.Player.Enable();
+    }
+
+    private void StartMove(InputAction.CallbackContext ctx)
+    {
+        valueX = ctx.ReadValue<float>();
+    }
+}
