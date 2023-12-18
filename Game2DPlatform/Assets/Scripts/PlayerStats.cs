479cdde2dca0c3bc81ccc0cf2e5126b83e3bbb71
fors3tti <152114883+Fors3tti@users.noreply.github.com>
2023-12-18 00:25:49 -0400

    Added Players Take Damage

--- Game2DPlatform/Assets/Scripts/PlayerStats.cs
+++ Game2DPlatform/Assets/Scripts/PlayerStats.cs
@@ -7,23 +7,46 @@ public class PlayerStats : MonoBehaviour
     public float maxHealth;
     public float health;
 
+    public bool canTakeDamage = true;
+
     // Start is called before the first frame update
     void Start()
     {
         health = maxHealth;
         TakeDamage(20);
+        TakeDamage(20);
     }
 
     public void TakeDamage(float damage)
     {
-        health -= damage;
-        //play hurt animation
-
-        if(health <= 0)
+        if (canTakeDamage)
         {
-            GetComponent<PolygonCollider2D>().enabled = false;
-            GetComponentInParent<GatherInput>().DisableControls();
-            Debug.Log("The Player is dead");
+            health -= damage;
+            //play hurt animation
+
+            if (health <= 0)
+            {
+                GetComponent<PolygonCollider2D>().enabled = false;
+                GetComponentInParent<GatherInput>().DisableControls();
+                Debug.Log("The Player is dead");
+            }
+
+            StartCoroutine(DamagePrevetion());
+        }
+    }
+
+    private IEnumerator DamagePrevetion()
+    {
+        canTakeDamage = false;
+        yield return new WaitForSeconds(0.15f);
+
+        if(health > 0)
+        {
+            canTakeDamage = true;
+        }
+        else
+        {
+            //play death animation
         }
     }
 }
