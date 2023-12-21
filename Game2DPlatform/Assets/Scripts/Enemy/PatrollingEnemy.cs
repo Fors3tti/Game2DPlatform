7658b4994e2e380699952460541c01b6b66d6663
fors3tti <152114883+Fors3tti@users.noreply.github.com>
2023-12-21 02:14:08 -0400

    Added Patrolling Enemies Ground and Wall Detection

--- Game2DPlatform/Assets/Scripts/Enemy/PatrollingEnemy.cs
+++ Game2DPlatform/Assets/Scripts/Enemy/PatrollingEnemy.cs
@@ -4,6 +4,18 @@ using UnityEngine;
 
 public class PatrollingEnemy : Enemy
 {
+    public float speed;
+    private int direction = -1;
+
+    public Transform groundCheck;
+    public Transform wallCheck;
+    public LayerMask layerToCheck;
+
+    private bool detectGround;
+    private bool detectWall;
+
+    public float radius;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -17,6 +29,25 @@ public class PatrollingEnemy : Enemy
     }
     private void FixedUpdate()
     {
-        rb.velocity = new Vector2(-1, rb.velocity.y);
+        Flip();
+        rb.velocity = new Vector2(direction * speed, rb.velocity.y);
+    }
+
+    private void Flip()
+    {
+        detectGround = Physics2D.OverlapCircle(groundCheck.position, radius, layerToCheck);
+        detectWall = Physics2D.OverlapCircle(wallCheck.position, radius, layerToCheck);
+
+        if(detectWall || detectGround == false)
+        {
+            direction *= -1;
+            transform.localScale = new Vector3(-transform.localScale.x, 1, 1);
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawWireSphere(groundCheck.position, radius);
+        Gizmos.DrawWireSphere(wallCheck.position, radius);
     }
 }
